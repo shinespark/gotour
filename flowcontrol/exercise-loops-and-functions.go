e6024c70ee9c07c69dbeb6fa16ed419eaadb2509
shinespark <shinespark64@gmail.com>
2018-09-21 18:40:42 +0900

    finish flowcontrol

---
+++ flowcontrol/exercise-loops-and-functions.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) (float64, float64) {
+
+	z := 1.0
+
+	for i := 1; i <= 10; i++ {
+		diff := (z*z - x) / (2 * z)
+
+		if z == z-diff {
+			return x, z
+		}
+
+		z -= diff
+	}
+
+	return x, z
+
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+
+	for x := 1; x <= 10; x++ {
+		fmt.Println(Sqrt(float64(x)))
+		fmt.Println(math.Sqrt(float64(x)))
+	}
+}
